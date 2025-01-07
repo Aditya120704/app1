1b9a3aec88bc94dc804f47c04dbfc9b5cd0ccf6c
Aditya120704 <adityavaghela12@gmail.com>
2025-01-07 15:26:01 +0530

    First Commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.firebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btn:MaterialButton
+    lateinit var data:TextInputEditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {git
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn=findViewById(R.id.my_button)
+        data=findViewById(R.id.input_data)
+
+
+        btn.setOnClickListener {
+            val user_data:String = data.text.toString().trim()
+
+            //Intent intent = new Intent()
+
+            val obj=Intent(this@MainActivity,second::class.java)
+            obj.putExtra("All Data",user_data)
+
+            startActivity(obj)
+            finish()
+
+
+
+
+        }
+    }
+}
